5323d0829aab225d1f670ada8ce9544ead5b5687
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-07-01 21:22:08 +0400

    grid design done!

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -1,5 +1,158 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container mx-auto px-4 pt-12">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-yellow-500 text-lg font-semibold">Popular movies</h2>
+            <div class="grid grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/ryjHVu68Hgk368sS8KpdzHapx3J.jpg" alt="loki" class="hover:opacity-75 transition ease-in-out duration-200">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-300">Loki</a>
+                        <div class="flex items-center text-gray-400 text-sm mt-1">
+                            <svg class="fill-current text-yellow-500 w-4" viewBox="0 0 24 24">
+                                <g data-name="Layer 2">
+                                    <path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star" />
+                                </g>
+                            </svg>
+                            <span class="ml-1">81%</span>
+                            <span class="mx-2">|</span>
+                            <span>June 9, 2021</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Drama, Sci-Fi, Fantasy
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
