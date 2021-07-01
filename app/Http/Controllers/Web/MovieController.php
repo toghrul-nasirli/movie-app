0755cf4fa64c24472ce6c53846612038e5668fc6
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-07-01 23:30:03 +0400

    popular movies added

--- app/Http/Controllers/Web/MovieController.php
+++ app/Http/Controllers/Web/MovieController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Http;
 
 class MovieController extends Controller
 {
     public function index()
     {
-        return view('movies.index');
+        $popularMovies = Http::withToken(config('services.tmdb.token'))
+            ->get('https://api.themoviedb.org/3/movie/popular')
+            ->json()['results'];
+
+        return view('movies.index', compact('popularMovies'));
     }
 
     public function show()
