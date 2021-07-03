1f54a824e2d26f39fc635f629baea70bf8de7104
Toghrul Nasirli <toghrulnasirli111@gmail.com>
2021-07-03 18:24:31 +0400

    search by pressing '/' feature removed for browser compatibility issue

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,5 +1,5 @@
 <div x-data="{ isOpen: true }" @click.away="isOpen = false" @keydown.escape.window="isOpen = false" @keydown.shift.tab="isOpen = false" @keydown="isOpen = true" class="relative mt-3 md:mt-0">
-    <input wire:model.debounce.500ms="search" x-ref="search" @keydown.window.prevent="if(event.keyCode === 191) { $refs.search.focus(); }" @focus="isOpen = true" type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search (Press '/')">
+    <input wire:model.debounce.500ms="search" @focus="isOpen = true" type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline" placeholder="Search...">
     <div class="absolute top-0">
         <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24">
             <path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z" />
